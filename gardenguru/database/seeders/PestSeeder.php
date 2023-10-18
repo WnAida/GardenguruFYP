f81bcfbe0348b6f713cdcbb6e547b78794f9cb2c
Wan Aida <wnaida05@gmail.com>
2023-10-19 00:26:52 +0800

    DATABASE
    seeder,model,factory

---
+++ gardenguru/database/seeders/PestSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PestSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
