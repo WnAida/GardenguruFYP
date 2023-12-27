bb57563685bc1b97f5d13304cdd15d6c286bcce1
Wan Aida <wnaida05@gmail.com>
2023-12-27 16:26:44 +0800

    bank controller

--- app/Http/Controllers/v1/BankController.php
+++ app/Http/Controllers/v1/BankController.php
@@ -3,9 +3,20 @@
 namespace App\Http\Controllers\v1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\v1\BankResource;
+use App\Models\Bank;
+use App\Traits\ApiPaginatorTrait;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class BankController extends Controller
 {
-    //
+    use ApiPaginatorTrait;
+    public function index()
+    {
+        $take = request()->get('take', 1000);
+
+        $data = Bank::paginate($take);
+        return $this->return_paginated_api(true, Response::HTTP_OK, null, BankResource::collection($data), null, $this->apiPaginator($data));
+    }
 }
