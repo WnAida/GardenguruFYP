392b59ae6d4d214fed9fb9d2700a1c6c90c9f304
Wan Aida <wnaida05@gmail.com>
2024-01-09 19:43:46 +0800

    image

--- app/Http/Resources/v1/VegetableResource.php
+++ app/Http/Resources/v1/VegetableResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources\v1;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class VegetableResource extends JsonResource
 {
@@ -18,7 +19,7 @@ class VegetableResource extends JsonResource
             'name'=> $this->name,
             'description'=> $this->description,
             'note'=> $this->note,
-            'photo'=> $this->photo,
+            'photo'=> Storage::disk('public')->url($this->photo),
             'pests' => PestResource::collection($this->pests),
             'guidances' => GuidanceResource::collection($this->guidances),
         ];
